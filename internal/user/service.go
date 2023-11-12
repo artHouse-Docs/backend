52627e97f9c31f626e8965f33d5514523828ed06
Руслан Мельник <melnk300@gmail.com>
2023-11-12 23:26:37 +0500

    IDK

--- internal/user/service.go
+++ internal/user/service.go
@@ -3,6 +3,10 @@ package user
 import (
 	"context"
 	"errors"
+	"github.com/artHouse-Docs/backend/pkg/dto"
+	"github.com/artHouse-Docs/backend/pkg/grpcBridge"
+	"google.golang.org/grpc"
+	"log"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -10,12 +14,10 @@ import (
 	"github.com/artHouse-Docs/backend/pkg/hashing"
 )
 
-// handle request() {userId     register()}
-
 func (u *User) Register(ctx context.Context) (err error) {
-	coll, err := NewUserCollenction(ctx)
+	coll, err := NewUserCollection(ctx)
 	if err != nil {
-		return errors.New("database unavaliable")
+		return errors.New("database unavailable")
 	}
 
 	result, err := coll.InsertOne(ctx, bson.D{
@@ -26,18 +28,36 @@ func (u *User) Register(ctx context.Context) (err error) {
 	})
 
 	if err != nil {
-		return errors.New("server error")
+		return errors.New("user email already exists")
 	}
 
 	u.ID = result.InsertedID.(primitive.ObjectID).Hex()
+
+	cc := grpcBridge.CreateConnection()
+	defer func(cc *grpc.ClientConn) {
+		err := cc.Close()
+		if err != nil {
+			log.Fatal("Unexpected connection error\t", err.Error())
+		}
+	}(cc)
+
+	client := dto.NewAuthServiceClient(cc)
+
+	tokens, err := client.Login(context.Background(), &dto.Payload{Id: u.ID})
+	if err != nil {
+		log.Fatal("Unexpected token error\t", err.Error())
+	}
+
+	u.AccessToken = tokens.AccessToken
+	u.RefreshToken = tokens.RefreshToken
 
 	return nil
 }
 
 func (u *User) Login(ctx context.Context) (result bool, err error) {
-	coll, err := NewUserCollenction(ctx)
+	coll, err := NewUserCollection(ctx)
 	if err != nil {
-		return false, errors.New("database unavaliable")
+		return false, errors.New("database unavailable")
 	}
 
 	var userByEmail User
